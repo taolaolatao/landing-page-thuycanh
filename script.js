da97ab9547c4a56c660ba360188a205eed020447
unknown <taolaolataom@gmail.com>
2020-10-03 15:56:22 +0700

    Seconds Commit

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 $(document).ready(function () {
   "use-stricts";
 
+  ScrollOut({
+    once: true,
+    // targets: ".hydroponics-img",
+    // onShown: function (el) {
+    //   console.log(el);
+    // use the web animation API
+    // el.classList.add("slideLeft");
+    // el.animate([{ opacity: 0 }, { opacity: 1 }], 1000);
+    // },
+    // onHidden: function (el) {
+    // hide the element initially
+    // el.style.opacity = 0;
+    // },
+  });
+
   // Get the navbar
   const navbar = document.querySelector(".navbar");
 
@@ -33,11 +48,11 @@ $(document).ready(function () {
   scrollToSection("#project", "#section-project");
   scrollToSection("#testimonials", "#section-testimonials");
 
-  document
-    .getElementById("scroll-to-top")
-    .addEventListener("click", function () {
-      $("html, body").animate({ scrollTop: 0 }, "slow");
-    });
+  const scrollToTop = document.getElementById("scroll-to-top");
+
+  scrollToTop.addEventListener("click", function () {
+    $("html, body").animate({ scrollTop: 0 }, "slow");
+  });
   document.getElementById("contact").addEventListener("click", function (e) {
     e.preventDefault();
     $("html, body").animate({ scrollTop: document.body.scrollHeight }, "slow");
@@ -45,9 +60,9 @@ $(document).ready(function () {
 
   window.onscroll = function () {
     if ($(document).scrollTop() > 700) {
-      $("#scroll-to-top").addClass("show");
+      scrollToTop.classList.add("show");
     } else {
-      $("#scroll-to-top").removeClass("show");
+      scrollToTop.classList.remove("show");
     }
     navbarSticky();
   };
@@ -65,22 +80,92 @@ $(document).ready(function () {
     },
   });
 
-  let ourProduct = "";
-  for (let i = 0; i < 6; i++) {
-    ourProduct += ` 
-      <div class="col-12 col-sm-4 mt-4">
+  const products = [
+    {
+      title: "Card Title",
+      content: "Some quick example text to build on the card title and make up",
+      img: "./images/products/1.jpg",
+    },
+    {
+      title: "Card Title",
+      content: "Some quick example text to build on the card title and make up",
+      img: "./images/products/2.jpg",
+    },
+    {
+      title: "Card Title",
+      content: "Some quick example text to build on the card title and make up",
+      img: "./images/products/3.jpg",
+    },
+    {
+      title: "Card Title",
+      content: "Some quick example text to build on the card title and make up",
+      img: "./images/products/4.jpg",
+    },
+    {
+      title: "Card Title",
+      content: "Some quick example text to build on the card title and make up",
+      img: "./images/products/5.jpg",
+    },
+    {
+      title: "Card Title",
+      content: "Some quick example text to build on the card title and make up",
+      img: "./images/products/6.jpg",
+    },
+    {
+      title: "Card Title",
+      content: "Some quick example text to build on the card title and make up",
+      img: "./images/products/7.jpg",
+    },
+    {
+      title: "Card Title",
+      content: "Some quick example text to build on the card title and make up",
+      img: "./images/products/8.jpg",
+    },
+    {
+      title: "Card Title",
+      content: "Some quick example text to build on the card title and make up",
+      img: "./images/products/9.jpg",
+    },
+    {
+      title: "Card Title",
+      content: "Some quick example text to build on the card title and make up",
+      img: "./images/products/10.jpg",
+    },
+    {
+      title: "Card Title",
+      content: "Some quick example text to build on the card title and make up",
+      img: "./images/products/11.jpg",
+    },
+  ];
+
+  function loadProducts(products, start = 0, end = 0) {
+    let ourProduct = "";
+    const arr = end > 0 ? products.slice(start, end) : products;
+    for (const product of arr) {
+      ourProduct += `
+      <div class="col-12 col-sm-8 col-md-6 col-lg-4 mt-4">
         <div class="card product" style="width: 18rem">
-          <img src="./images/1.jpg" class="card-img-top" alt="..." />
+          <img src="${product.img}" class="card-img-top" alt="" />
           <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">
-              Some quick example text to build on the card title and make up
-              the bulk of the card's content.
-            </p>
+            <h5 class="card-title">${product.title}</h5>
+            <p class="card-text">${product.content}</p>
           </div>
         </div>
       </div>`;
+    }
+    return ourProduct;
   }
 
-  document.querySelector("#our-product").innerHTML = ourProduct;
+  const btnLoadMore = document.getElementById("btn-see-more-product");
+  const containProducts = document.getElementById("our-product");
+
+  containProducts.innerHTML = loadProducts(products, 0, 6);
+
+  btnLoadMore.addEventListener("click", function () {
+    containProducts.insertAdjacentHTML(
+      "beforeend",
+      loadProducts(products, 6, 11)
+    );
+    btnLoadMore.style.display = "none";
+  });
 });
